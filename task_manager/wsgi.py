17da2f85e2fef10ea01d7e43d1b83fbd5dcc96da
arina.khokhlova <arina.khokhlova@KHOKHLOVAAV-MB.local>
2025-05-16 18:27:50 +0300

    Update wsgi

--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -1,21 +1,6 @@
-"""
-WSGI config for task_manager project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
-
-# application = get_wsgi_application() delete
-
-application = get_wsgi_application()
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
+application = WhiteNoise(get_wsgi_application())
