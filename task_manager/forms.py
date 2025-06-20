ee0184d9d7d5280e18159018c7e535a0641699ab
arina.khokhlova <arina.khokhlova@KHOKHLOVAAV-MB.local>
2025-06-20 16:22:09 +0300

    Update forms

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -2,12 +2,29 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class CustomLoginForm(AuthenticationForm):
-    """Authentication form with Bootstrap styling."""
+    """Authentication form with Bootstrap styling and accessibility support."""
+
+    BASE_CSS_CLASS = 'form-control'
+    BACKGROUND_CLASS = 'bg-secondary bg-opacity-50'
+    BORDER_CLASS = 'border-secondary'
+
     def __init__(self, *args, **kwargs):
         """Initializes the login form with custom Bootstrap styles."""
         super().__init__(*args, **kwargs)
 
-        bs_cls = 'form-control bg-secondary bg-opacity-50 border-secondary'
-        for field in self.fields.values():
-            field.widget.attrs['class'] = bs_cls
-            field.widget.attrs['placeholder'] = field.label
+        bs_cls = (
+            f'{self.BASE_CSS_CLASS} '
+            f'{self.BACKGROUND_CLASS} '
+            f'{self.BORDER_CLASS}'
+        )
+        
+        for field_name, field in self.fields.items():
+            existing_class = field.widget.attrs.get('class', '')
+            field.widget.attrs['class'] = f'{existing_class} {bs_cls}'.strip()
+            
+            field.widget.attrs['placeholder'] = _(field.label)
+            
+            field.widget.attrs.update({
+                'aria-label': _(field.label),
+                'id': f'id_{field_name}'
+            })
