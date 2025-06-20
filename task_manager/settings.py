9c4ca39331041679ff4c05e7121227ffefca932e
arina.khokhlova <arina.khokhlova@KHOKHLOVAAV-MB.local>
2025-06-20 13:21:34 +0300

    Update DB settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -97,7 +97,6 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
