320407b9de2c2c23e78f0e15eccdae9e0ebf56c6
arina.khokhlova <arina.khokhlova@KHOKHLOVAAV-MB.local>
2025-06-22 18:37:26 +0300

    Update models

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,18 +4,25 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    """Custom user model with required first and last names."""
+    """
+    Custom user model extending Django's AbstractUser.
+    Requires first_name and last_name fields to be non-empty.
+    """
     first_name = models.CharField(
         max_length=150,
         blank=False,
-        verbose_name=_('First Name')
+        verbose_name=_('First Name'),
+        help_text=_('Required. 150 characters or fewer.')
     )
     last_name = models.CharField(
         max_length=150,
         blank=False,
-        verbose_name=_('Last Name')
+        verbose_name=_('Last Name'),
+        help_text=_('Required. 150 characters or fewer.')
     )
+
     USERNAME_FIELD = 'username'
 
     def __str__(self):
+        """Display user as 'First Last'."""
         return f'{self.first_name} {self.last_name}'
